7495388af7b6a1eac1f2e8bd6c9375a9e62c087a
MD ASIF <56890311+imasifuddin@users.noreply.github.com>
2019-10-29 22:22:11 +0530

    Add files via upload

---
+++ even11.py
@@ -0,0 +1,30 @@
+
+f=open("integer value.txt",'r')
+
+for i in f:
+    if int(i)%2==0:
+        f1=open("even.txt",'a')
+        f1.write(i)
+        f1.close()
+    else:
+        f2=open("odd.txt",'a')
+        f2.write(i)
+        f2.close()
+
+f.close()
+
+print("Before spliting the numbers are")
+f=open("integer value.txt",'r')
+print(f.read())
+
+print("After spliting, even numbers are")
+f1=open("even.txt",'r')
+print(f1.read())
+
+print("After spliting, odd numbers are")
+f2=open("odd.txt",'r')
+print(f2.read())
+
+f.close()
+f1.close()
+f2.close()
