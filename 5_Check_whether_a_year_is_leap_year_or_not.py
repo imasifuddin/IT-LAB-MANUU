89a4feb5c2c787d04ed45bb0b842da624c45ea12
MD ASIF <56890311+imasifuddin@users.noreply.github.com>
2019-11-28 13:03:03 +0530

    Update 5_Check_whether_a_year_is_leap_year_or_not.py

--- 5_Check_whether_a_year_is_leap_year_or_not.py
+++ 5_Check_whether_a_year_is_leap_year_or_not.py
@@ -1,14 +1,14 @@
 
 a=int(input("Enter any year what you want="))
 
-if a%100:
-      if a%400:
+if a%100==0:
+      if a%400==0:
           print("{0} is a leap year".format(a))
       else:
           print("{0} is not a leap year".format(a))
 
 else:
-      if a%4:
+      if a%4==0:
           print("{0} is a leap year".format(a))
       else:
           print("{0} is not a leap year".format(a))
